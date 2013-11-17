e40800e7e7abd833dcecfdafad64e60856b63e14
c9s <cornelius.howl@gmail.com>
2013-11-18 01:43:45 +0800

    Add disk info

---
+++ linux/disk.go
@@ -0,0 +1,24 @@
+package linux
+
+import (
+	"syscall"
+)
+
+type Disk struct {
+	All  uint64 `json:"all"`
+	Used uint64 `json:"used"`
+	Free uint64 `json:"free"`
+}
+
+func disk(path string) (*Disk, error) {
+	fs := syscall.Statfs_t{}
+	err := syscall.Statfs(path, &fs)
+	if err != nil {
+		return nil, err
+	}
+	disk := Disk{}
+	disk.All = fs.Blocks * uint64(fs.Bsize)
+	disk.Free = fs.Bfree * uint64(fs.Bsize)
+	disk.Used = disk.All - disk.Free
+	return &disk, nil
+}
