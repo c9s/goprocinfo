48a36cf79b6bc5232715463c1e959e942ba00c9a
Pierre-Alain Riviere <pariviere@ippon.fr>
2014-11-23 10:50:35 +0400

    Add /proc/net/dev support

---
+++ linux/network_stat_test.go
@@ -0,0 +1,25 @@
+package linux
+
+import "testing"
+import "reflect"
+
+func TestNetworkStat(t *testing.T) {
+
+        var expected = []NetworkStat{
+                {"eth0",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {"lo",870813,8693,0,0,0,0,0,0,870813,8693,0,0,0,0,0,0},
+                {"virbr0",0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                {"wlan0",1163823097,838432,0,0,0,0,0,0,73047180,641124,0,0,0,0,0,0},
+        }
+
+        networkStat, err := ReadNetworkStat("proc/net_dev")
+	if err != nil {
+		t.Fatal("network stat read fail", err)
+	}
+
+	t.Logf("%+v", networkStat)
+
+        if !reflect.DeepEqual(networkStat, expected) {
+                t.Error("not equal to expected")
+        }
+}
