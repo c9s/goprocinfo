ff91e490e8460e776889511b117108f244f0a348
Thomas LE ROUX <github@november-eleven.fr>
2015-03-11 13:09:28 +0100

    Add list & max pid on process

--- linux/process.go
+++ linux/process.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"path/filepath"
 	"strconv"
+	"strings"
 )
 
 type Process struct {
@@ -48,6 +49,51 @@ func ReadProcessStatus(path string) (*ProcessStatus, error) {
 	status := ProcessStatus{}
 	_ = b
 	return &status, nil
+}
+
+func ReadMaxPID(path string) (uint64, error) {
+
+	b, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		return 0, err
+	}
+
+	s := strings.TrimSpace(string(b))
+
+	i, err := strconv.ParseUint(s, 10, 0)
+
+	if err != nil {
+		return 0, err
+	}
+
+	return i, nil
+
+}
+
+func ListPID(path string, max uint64) ([]uint64, error) {
+
+	l := make([]uint64, 0, 5)
+
+	for i := uint64(1); i <= max; i++ {
+
+		p := filepath.Join(path, strconv.FormatUint(i, 10))
+
+		s, err := os.Stat(p)
+
+		if err != nil && !os.IsNotExist(err) {
+			return nil, err
+		}
+
+		if err != nil || !s.IsDir() {
+			continue
+		}
+
+		l = append(l, i)
+
+	}
+
+	return l, nil
 }
 
 // process info reader
