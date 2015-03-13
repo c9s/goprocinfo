1267750fdfd6850e92bd0271f8188736c59f020f
Thomas LE ROUX <github@november-eleven.fr>
2015-03-13 10:54:39 +0100

    Add process stat & Fix integer parsing

--- linux/process.go
+++ linux/process.go
@@ -40,7 +40,61 @@ type ProcessStatm struct {
 	Dirty    uint64 `json:"dirty"`    // dirty pages (unused in Linux 2.6)
 }
 
-type ProcessStat struct{}
+// Status information about the process.
+type ProcessStat struct {
+	Pid                 uint64 `json:"pid"`
+	Comm                string `json:"comm"`
+	State               string `json:"state"`
+	Ppid                int64  `json:"ppid"`
+	Pgrp                int64  `json:"pgrp"`
+	Session             int64  `json:"session"`
+	TtyNr               int64  `json:"tty_nr"`
+	Tpgid               int64  `json:"tpgid"`
+	Flags               uint64 `json:"flags"`
+	Minflt              uint64 `json:"minflt"`
+	Cminflt             uint64 `json:"cminflt"`
+	Majflt              uint64 `json:"majflt"`
+	Cmajflt             uint64 `json:"cmajflt"`
+	Utime               uint64 `json:"utime"`
+	Stime               uint64 `json:"stime"`
+	Cutime              int64  `json:"cutime"`
+	Cstime              int64  `json:"cstime"`
+	Priority            int64  `json:"priority"`
+	Nice                int64  `json:"nice"`
+	NumThreads          int64  `json:"num_threads"`
+	Itrealvalue         int64  `json:"itrealvalue"`
+	Starttime           uint64 `json:"starttime"`
+	Vsize               uint64 `json:"vsize"`
+	Rss                 int64  `json:"rss"`
+	Rsslim              uint64 `json:"rsslim"`
+	Startcode           uint64 `json:"startcode"`
+	Endcode             uint64 `json:"endcode"`
+	Startstack          uint64 `json:"startstack"`
+	Kstkesp             uint64 `json:"kstkesp"`
+	Kstkeip             uint64 `json:"kstkeip"`
+	Signal              uint64 `json:"signal"`
+	Blocked             uint64 `json:"blocked"`
+	Sigignore           uint64 `json:"sigignore"`
+	Sigcatch            uint64 `json:"sigcatch"`
+	Wchan               uint64 `json:"wchan"`
+	Nswap               uint64 `json:"nswap"`
+	Cnswap              uint64 `json:"cnswap"`
+	ExitSignal          int64  `json:"exit_signal"`
+	Processor           int64  `json:"processor"`
+	RtPriority          uint64 `json:"rt_priority"`
+	Policy              uint64 `json:"policy"`
+	DelayacctBlkioTicks uint64 `json:"delayacct_blkio_ticks"`
+	GuestTime           uint64 `json:"guest_time"`
+	CguestTime          int64  `json:"cguest_time"`
+	StartData           uint64 `json:"start_data"`
+	EndData             uint64 `json:"end_data"`
+	StartBrk            uint64 `json:"start_brk"`
+	ArgStart            uint64 `json:"arg_start"`
+	ArgEnd              uint64 `json:"arg_end"`
+	EnvStart            uint64 `json:"env_start"`
+	EnvEnd              uint64 `json:"env_end"`
+	ExitCode            int64  `json:"exit_code"`
+}
 
 func ReadProcess(pid uint64, path string) (*Process, error) {
 
@@ -52,12 +106,12 @@ func ReadProcess(pid uint64, path string) (*Process, error) {
 
 	process := Process{}
 
-	statPath := filepath.Join(p, "stat")
 	statusPath := filepath.Join(p, "status")
 
 	var err error
 	var io *ProcessIO
 	var statm *ProcessStatm
+	var stat *ProcessStat
 
 	if io, err = ReadProcessIO(filepath.Join(p, "io")); err != nil {
 		return nil, err
@@ -67,11 +121,15 @@ func ReadProcess(pid uint64, path string) (*Process, error) {
 		return nil, err
 	}
 
-	_ = statPath
+	if stat, err = ReadProcessStat(filepath.Join(p, "stat")); err != nil {
+		return nil, err
+	}
+
 	_ = statusPath
 
 	process.IO = *io
 	process.Statm = *statm
+	process.Stat = *stat
 
 	return &process, nil
 }
@@ -110,7 +168,7 @@ func ReadProcessIO(path string) (*ProcessIO, error) {
 		l := strings.Split(line, ": ")
 
 		k := l[0]
-		v, err := strconv.ParseUint(l[1], 10, 0)
+		v, err := strconv.ParseUint(l[1], 10, 64)
 
 		if err != nil {
 			return nil, err
@@ -153,9 +211,9 @@ func ReadProcessStatm(path string) (*ProcessStatm, error) {
 
 	var n uint64
 
-	for i := 0; i < 7; i++ {
+	for i := 0; i < len(f); i++ {
 
-		if n, err = strconv.ParseUint(f[i], 10, 0); err != nil {
+		if n, err = strconv.ParseUint(f[i], 10, 64); err != nil {
 			return nil, err
 		}
 
@@ -175,9 +233,235 @@ func ReadProcessStatm(path string) (*ProcessStatm, error) {
 		case 6:
 			statm.Dirty = n
 		}
+
 	}
 
 	return &statm, nil
+}
+
+func ReadProcessStat(path string) (*ProcessStat, error) {
+
+	b, err := ioutil.ReadFile(path)
+
+	if err != nil {
+		return nil, err
+	}
+
+	s := string(b)
+	f := strings.Fields(s)
+
+	stat := ProcessStat{}
+
+	for i := 0; i < len(f); i++ {
+		switch i {
+		case 0:
+			if stat.Pid, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 1:
+			stat.Comm = f[i]
+		case 2:
+			stat.State = f[i]
+		case 3:
+			if stat.Ppid, err = strconv.ParseInt(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 4:
+			if stat.Pgrp, err = strconv.ParseInt(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 5:
+			if stat.Session, err = strconv.ParseInt(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 6:
+			if stat.TtyNr, err = strconv.ParseInt(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 7:
+			if stat.Tpgid, err = strconv.ParseInt(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 8:
+			if stat.Flags, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 9:
+			if stat.Minflt, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 10:
+			if stat.Cminflt, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 11:
+			if stat.Majflt, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 12:
+			if stat.Cmajflt, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 13:
+			if stat.Utime, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 14:
+			if stat.Stime, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 15:
+			if stat.Cutime, err = strconv.ParseInt(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 16:
+			if stat.Cstime, err = strconv.ParseInt(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 17:
+			if stat.Priority, err = strconv.ParseInt(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 18:
+			if stat.Nice, err = strconv.ParseInt(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 19:
+			if stat.NumThreads, err = strconv.ParseInt(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 20:
+			if stat.Itrealvalue, err = strconv.ParseInt(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 21:
+			if stat.Starttime, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 22:
+			if stat.Vsize, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 23:
+			if stat.Rss, err = strconv.ParseInt(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 24:
+			if stat.Rsslim, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 25:
+			if stat.Startcode, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 26:
+			if stat.Endcode, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 27:
+			if stat.Startstack, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 28:
+			if stat.Kstkesp, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 29:
+			if stat.Kstkeip, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 30:
+			if stat.Signal, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 31:
+			if stat.Blocked, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 32:
+			if stat.Sigignore, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 33:
+			if stat.Sigcatch, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 34:
+			if stat.Wchan, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 35:
+			if stat.Nswap, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 36:
+			if stat.Cnswap, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 37:
+			if stat.ExitSignal, err = strconv.ParseInt(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 38:
+			if stat.Processor, err = strconv.ParseInt(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 39:
+			if stat.RtPriority, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 40:
+			if stat.Policy, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 41:
+			if stat.DelayacctBlkioTicks, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 42:
+			if stat.GuestTime, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 43:
+			if stat.CguestTime, err = strconv.ParseInt(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 44:
+			if stat.StartData, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 45:
+			if stat.EndData, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 46:
+			if stat.StartBrk, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 47:
+			if stat.ArgStart, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 48:
+			if stat.ArgEnd, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 49:
+			if stat.EnvStart, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 50:
+			if stat.EnvEnd, err = strconv.ParseUint(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		case 51:
+			if stat.ExitCode, err = strconv.ParseInt(f[i], 10, 64); err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return &stat, nil
 }
 
 func ReadMaxPID(path string) (uint64, error) {
@@ -190,7 +474,7 @@ func ReadMaxPID(path string) (uint64, error) {
 
 	s := strings.TrimSpace(string(b))
 
-	i, err := strconv.ParseUint(s, 10, 0)
+	i, err := strconv.ParseUint(s, 10, 64)
 
 	if err != nil {
 		return 0, err
--- linux/process_test.go
+++ linux/process_test.go
@@ -35,7 +35,7 @@ func TestReadProcessStatm(t *testing.T) {
 	statm, err := ReadProcessStatm("proc/3323/statm")
 
 	if err != nil {
-		t.Fatal("process io read fail", err)
+		t.Fatal("process statm read fail", err)
 	}
 
 	expected := &ProcessStatm{
@@ -53,6 +53,69 @@ func TestReadProcessStatm(t *testing.T) {
 	}
 
 	t.Logf("%+v", statm)
+
+}
+
+func TestReadProcessStat(t *testing.T) {
+
+	stat, err := ReadProcessStat("proc/3323/stat")
+
+	if err != nil {
+		t.Fatal("process stat read fail", err)
+	}
+
+	expected := &ProcessStat{
+		Pid:                 3323,
+		Comm:                "(proftpd)",
+		State:               "S",
+		Ppid:                1,
+		Pgrp:                3323,
+		Session:             3323,
+		TtyNr:               0,
+		Tpgid:               -1,
+		Flags:               4202816,
+		Minflt:              1311,
+		Cminflt:             57367,
+		Majflt:              0,
+		Cmajflt:             1,
+		Utime:               23,
+		Stime:               58,
+		Cutime:              24,
+		Cstime:              49,
+		Priority:            20,
+		Nice:                0,
+		NumThreads:          1,
+		Itrealvalue:         0,
+		Starttime:           2789,
+		Vsize:               16601088,
+		Rss:                 522,
+		Rsslim:              4294967295,
+		Startcode:           134512640,
+		Endcode:             135222176,
+		Startstack:          3217552592,
+		Kstkesp:             3217551836,
+		Kstkeip:             4118799382,
+		Signal:              0,
+		Blocked:             0,
+		Sigignore:           272633856,
+		Sigcatch:            8514799,
+		Wchan:               0,
+		Nswap:               0,
+		Cnswap:              0,
+		ExitSignal:          17,
+		Processor:           7,
+		RtPriority:          0,
+		Policy:              0,
+		DelayacctBlkioTicks: 1,
+		GuestTime:           0,
+		CguestTime:          0,
+	}
+
+	if !reflect.DeepEqual(stat, expected) {
+		t.Error("not equal to expected", expected)
+	}
+
+	t.Logf("%+v", stat)
 
 }
 
@@ -76,8 +139,52 @@ func TestReadProcess(t *testing.T) {
 			Data:     286,
 			Dirty:    0,
 		},
-		Stat: ProcessStat{},
-
+		Stat: ProcessStat{
+			Pid:                 3323,
+			Comm:                "(proftpd)",
+			State:               "S",
+			Ppid:                1,
+			Pgrp:                3323,
+			Session:             3323,
+			TtyNr:               0,
+			Tpgid:               -1,
+			Flags:               4202816,
+			Minflt:              1311,
+			Cminflt:             57367,
+			Majflt:              0,
+			Cmajflt:             1,
+			Utime:               23,
+			Stime:               58,
+			Cutime:              24,
+			Cstime:              49,
+			Priority:            20,
+			Nice:                0,
+			NumThreads:          1,
+			Itrealvalue:         0,
+			Starttime:           2789,
+			Vsize:               16601088,
+			Rss:                 522,
+			Rsslim:              4294967295,
+			Startcode:           134512640,
+			Endcode:             135222176,
+			Startstack:          3217552592,
+			Kstkesp:             3217551836,
+			Kstkeip:             4118799382,
+			Signal:              0,
+			Blocked:             0,
+			Sigignore:           272633856,
+			Sigcatch:            8514799,
+			Wchan:               0,
+			Nswap:               0,
+			Cnswap:              0,
+			ExitSignal:          17,
+			Processor:           7,
+			RtPriority:          0,
+			Policy:              0,
+			DelayacctBlkioTicks: 1,
+			GuestTime:           0,
+			CguestTime:          0,
+		},
 		IO: ProcessIO{
 			RChar:               3865585,
 			WChar:               183294,
